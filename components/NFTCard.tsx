57e59943760d69b6037dd926e23499a8c79af5fb
Akarsh Shrivastava <akarsh@metafic.co>
2022-09-28 20:45:09 +0530

    Added Cards in home page

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
-const NFTCard = () => {
+import { COLORS, SIZES, SHADOWS, assets  } from '../constants'
+import { nftCard } from '../utils/styles'
+import { nftCardProps } from '../utils/interface'
+import CircleButton from './CircleButton'
+
+const NFTCard = ({ data }: nftCardProps) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
+    <View style={nftCard.background} >
+      <View style={nftCard.card}>
+        <Image 
+        source={data.image} 
+        resizeMode="cover"
+        style={nftCard.image}
+        />
+        <CircleButton />
+      </View>
       <Text>NFTCard</Text>
     </View>
   )
 }
+
 
 export default NFTCard
